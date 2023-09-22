d6a6c3369dc1015cf3dfb1c00fdcc40c954ac97a
Miikka Seppi <miikka.seppi123@gmail.com>
2023-09-23 02:52:58 +0300

    Added basic login function (not ready yet, site wont work)

--- routes.py
+++ routes.py
@@ -1,5 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, session
 from os import urandom
+from visits import login
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["post"])
+def login():
+    name = request.form["name"]
+    password = request.form["password"]
+    role = request.form["role"]
+    if login(name, password, role):
+        return render_template("index.html")
+    else:
+        return render_template("error.html", message="Väärä salasana tai käyttäjätunnus")
 
