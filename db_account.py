4ed11d73c33fb349c5c357a26e15e9b3d4eaac27
mseppi <miikka.seppi123@gmail.com>
2024-11-28 20:23:35 +0200

    add cryptography vulnerability

--- db_account.py
+++ db_account.py
@@ -4,7 +4,7 @@ Contains functions to handle user accounts in database
 from os import urandom
 from db import db
 from flask import session, abort
-from werkzeug.security import check_password_hash, generate_password_hash
+# from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy import text
 
 def login(username, password):
@@ -14,7 +14,8 @@ def login(username, password):
     user = result.fetchone()
     print("User:")
     if user is not None:
-        if check_password_hash(user[0], password):
+        #if check_password_hash(user[0], password):
+        if user[0] == password:
             session["user_id"] = user[1]
             session["username"] = username
             session["csrf_token"] = urandom(16).hex()
@@ -24,10 +25,10 @@ def login(username, password):
 
 def register(username, password):
     """Register a new user"""
-    hash_value = generate_password_hash(password)
+    # hash_value = generate_password_hash(password)
     try:
         sql = text("INSERT INTO users (username, pword) VALUES (:username, :pword)")
-        db.session.execute(sql, {"username":username, "pword":hash_value})
+        db.session.execute(sql, {"username":username, "pword":password})
         sql2 = text("INSERT INTO profile (username) VALUES (:username)")
         db.session.execute(sql2, {"username":username})
         db.session.commit()
@@ -38,7 +39,7 @@ def register(username, password):
 def check_csrf(csrf_token):
     """Check if the CSRF token is valid"""
     #if session["csrf_token"] != csrf_token:
-     #   abort(403)
+    #    abort(403)
     pass
 
 def logout():
